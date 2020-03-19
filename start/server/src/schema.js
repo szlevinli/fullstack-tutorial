972642afc3b1a5312c2f3e8ac1a60fb6ad43bfce
levin <3259963@qq.com>
2020-03-19 10:02:59 +0000

    first step

---
+++ start/server/src/schema.js
@@ -0,0 +1,53 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean
+  }
+
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
+
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
+
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+
+  type Query {
+    launches: [Launch]!
+    launch(id: ID!): Launch
+    me: User
+  }
+
+  type Mutation {
+    bookTrips(launchIDs: [ID]!): TripUpdateResponse!
+    cancelTrips(launchID: ID!): TripUpdateResponse!
+    login(email: String): String # login token
+  }
+`;
+
+module.exports = typeDefs;
